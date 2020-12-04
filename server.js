93a5ba8b77921e32298a41da646773edf35d1e5d
xuewenwen <782877160@qq.com>
2020-12-05 04:04:33 +0800

    node.js初体验

--- server.js
+++ server.js
@@ -24,14 +24,18 @@ var server = http.createServer(function (request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`二哈`)
+        response.write(`<DOCTYPE html>
+                      <head><link rel="stylesheet" href="/x"</head>
+                      <body>我是红色的字</body>
+        `)
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
         response.write(`body{color: red;}`)
         response.end()
-    } else {
+    }
+    else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(`你输入的路径不存在对应的内容`)
